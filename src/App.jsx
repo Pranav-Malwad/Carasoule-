0bdbefdb4af81bb9a601856517f699a7eed7346c
pranav Malwad <malwadpranav@gmail.com>
2024-02-08 18:40:15 +0530

    project done

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  let [counter, setcounter] = useState(0);
+  function handleNextButtonClick() {
+    {
+      counter == data.length - 1 ? (counter = 0) : setcounter(counter + 1);
+    }
+  }
+  function handlePreviousButtonClick() {
+    {
+      counter == 0 ? (counter = data.length) : setcounter(counter - 1);
+    }
+  }
+  let data = [
+    "https://images.unsplash.com/photo-1494500764479-0c8f2919a3d8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1511884642898-4c92249e20b6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1683009427590-dd987135e66c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/34/BA1yLjNnQCI1yisIZGEi_2013-07-16_1922_IMG_9873.jpg?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1429704658776-3d38c9990511?q=80&w=1979&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1413752362258-7af2a667b590?q=80&w=1776&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  ];
+
+  return (
+    <>
+      <div className="carasoule">
+        <button className="btn prev " onClick={handlePreviousButtonClick}>
+          &lt;
+        </button>
+        <img src={data[counter]} alt="" />
+        <button className="btn next" onClick={handleNextButtonClick}>
+          &gt;
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
